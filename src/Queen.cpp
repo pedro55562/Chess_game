42181455c5548350b3adb8633f184566181e02ee
pedro55562 <pedro55562.ufmg.br>
2023-10-09 16:13:42 -0700

    atualizando logica da rainha

--- src/Queen.cpp
+++ src/Queen.cpp
@@ -16,5 +16,5 @@ Queen::Queen(int QueenColor) : Piece(QUEEN, QueenColor) {}
 bool Queen::isValidQueenMove(int fromX, int fromY, int toX, int toY) const{
     int dx = abs(fromX - toX);
     int dy = abs(fromY - toY);
-    return (dx == dy)||(dx == 1 && dy == 0)||(dx == 0 && dy == 1)||( dx == 1 && dy == 1)||(dx != 0 && dy == 0)||(dx == 0 && dy != 0);
+    return (dx == dy)||(dx != 0 && dy == 0)||(dx == 0 && dy != 0);
 }
